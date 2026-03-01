5bdbadcac254961fef1fdb89b14fa3b469991bbb
Henrick Lin <3098568188@qq.com>
2026-03-01 22:16:57 +1100

    Add project URL to English and Chinese message files, and update Projects component to display project and repository links more clearly.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -24,46 +24,34 @@ const Projects: FC = () => {
         {projectsData.map((project, index) => (
           <div key={index} className="bg-black/[.05] dark:bg-white/[.06] p-2 rounded-lg">
             <div className="flex justify-between items-start mb-1">
-              <h3 className="text-lg font-semibold">
-                {project.title}
-                {(project.repoName || project.projectUrl) && (
-                  <>
-                    {' ('}
-                    <a
-                      href={project.repoUrl}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-indigo-600"
-                    >
-                      {project.repoName}
-                    </a>
-                    <a
-                      href={project.projectUrl}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-indigo-600 hover:underline"
-                    >
-                      {project.siteName}
-                    </a>
-                    {')'}
-                  </>
-                )}
-              </h3>
+              <h3 className="text-lg font-semibold">{project.title}</h3>
               <p className="text-xs text-gray-600 dark:text-gray-400">{project.period}</p>
             </div>
             <div className="text-xs text-gray-600 dark:text-gray-400 mb-1">
               <span className="flex flex-col mb-1">{project.technologies.join(', ')}</span>
-              {project.repoUrl && (
-                <a
-                  href={project.repoUrl}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="hover:underline"
-                >
-                  {project.repoUrl}
-                </a>
-              )}
-              {project.projectUrl && <span className="mr-2 text-xs">{project.projectUrl}</span>}
+              <div className="flex gap-2">
+                {project.projectUrl && (
+                  <a
+                    href={project.projectUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:underline"
+                  >
+                    {project.projectUrl}
+                  </a>
+                )}
+                {project.projectUrl && project.repoUrl && <span>|</span>}
+                {project.repoUrl && (
+                  <a
+                    href={project.repoUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:underline"
+                  >
+                    {project.repoUrl}
+                  </a>
+                )}
+              </div>
             </div>
             <ul className="list-disc list-inside space-y-1 text-xs">
               {project.description.map((item, i) => (
